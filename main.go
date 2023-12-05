d36ea0f3ebaf5919c2847e8005f22b463a283d22
Kavindu <kavindu@192.168.1.4>
2023-12-05 08:35:31 +0530

    add merkle tree

--- main.go
+++ main.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"chunk/streaming"
+	"crypto/sha256"
 	"fmt"
 )
+
+type MerkleNode struct {
+	Hash  string
+	Left  *MerkleNode
+	Right *MerkleNode
+}
+
+func calculateHash(data string) string {
+	hash := sha256.Sum256([]byte(data))
+	return fmt.Sprintf("%x", hash)
+}
+
+func buildMerkleTree(data []string) *MerkleNode {
+
+	if len(data) == 1 {
+		return &MerkleNode{Hash: calculateHash(data[0]), Left: nil, Right: nil}
+	}
+
+	mid := len(data) / 2
+	left := buildMerkleTree(data[:mid])
+	right := buildMerkleTree(data[mid:])
+	return &MerkleNode{Hash: calculateHash(left.Hash + right.Hash), Left: left, Right: right}
+}
+
+func printTree(node *MerkleNode, indent string) {
+	if node != nil {
+		fmt.Println(indent+"Hash:", node.Hash)
+		if node.Left != nil {
+			printTree(node.Left, indent+"  ")
+		}
+		if node.Right != nil {
+			printTree(node.Right, indent+"  ")
+		}
+	}
+}
 
 func main() {
 	inputFile := "test.jpg"
@@ -16,13 +52,22 @@ func main() {
 		return
 	}
 
-	// Retrieve and concatenate the chunks to reconstruct the original file
-	outputFileName := "new.jpg"
-	err = streaming.RetrieveChunks(chunkNames, outputFileName)
-	if err != nil {
-		fmt.Println("Error retrieving chunks:", err)
-		return
-	}
+	root := buildMerkleTree(chunkNames)
+	printTree(root, "")
+	fmt.Println("Root Hash:", root.Hash)
 
-	fmt.Println("File split and retrieved successfully!")
+	// root := merkle.MerkleNode {
+
+	// }
+
+	// Retrieve and concatenate the chunks to reconstruct the original file
+	// outputFileName := "new.jpg"
+	// err = streaming.RetrieveChunks(chunkNames, outputFileName)
+	// if err != nil {
+	// 	fmt.Println("Error retrieving chunks:", err)
+	// 	return
+	// }
+
+	// fmt.Println("File split and retrieved successfully!")
+
 }
--- merkle/merkle.go
+++ merkle/merkle.go
@@ -1,1 +1,58 @@
 package merkle
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type MerkleNode struct {
+	Hash  string
+	Left  *MerkleNode
+	Right *MerkleNode
+}
+
+func calculateHash(data string) string {
+	hash := sha256.Sum256([]byte(data))
+	return fmt.Sprintf("%x", hash)
+}
+
+func buildMerkleTree(data []string) *MerkleNode {
+
+	if len(data) == 1 {
+		return &MerkleNode{Hash: calculateHash(data[0]), Left: nil, Right: nil}
+	}
+
+	mid := len(data) / 2
+	left := buildMerkleTree(data[:mid])
+	right := buildMerkleTree(data[mid:])
+	return &MerkleNode{Hash: calculateHash(left.Hash + right.Hash), Left: left, Right: right}
+}
+
+func printTree(node *MerkleNode, indent string) {
+	if node != nil {
+		fmt.Println(indent+"Hash:", node.Hash)
+		if node.Left != nil {
+			printTree(node.Left, indent+"  ")
+		}
+		if node.Right != nil {
+			printTree(node.Right, indent+"  ")
+		}
+	}
+}
+
+func main() {
+
+	data := []string{"a", "b", "c"}
+
+	fmt.Println(data)
+	mid := len(data) / 2
+
+	fmt.Println("Mid: ", mid)
+
+	fmt.Println(data[:mid])
+	fmt.Println(data[mid:])
+
+	root := buildMerkleTree(data)
+	printTree(root, "")
+	fmt.Println("Root Hash:", root.Hash)
+}
